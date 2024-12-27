44e2ea3a14270e72c50b19e0313bc829798c9111
Milorad Karalic <miki.karalic@gmail.com>
2024-12-27 13:43:52 +0100

    Komentari koda vezanog za upload, poziv api-a i evente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+// importujemo SocketIoModule i SocketIoConfig iz ngx-socket-io biblioteke
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { FaApiService } from './services/fa-api.service';
 
+// konfiguracija za SocketIoModule koja sadrzi URL servera koji emituje dogadjaje
 const config: SocketIoConfig = { url: 'http://localhost:5000', options: {} };
 
 @NgModule({
@@ -17,6 +19,7 @@ const config: SocketIoConfig = { url: 'http://localhost:5000', options: {} };
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    // dodajemo SocketIoModule u imports niz i prosledjujemo mu konfiguraciju sa URL/om servera koji emituje dogadjaje
     SocketIoModule.forRoot(config)
   ],
   providers: [FaApiService],
--- src/app/services/fa-api.service.ts
+++ src/app/services/fa-api.service.ts
@@ -7,17 +7,22 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class FaApiService {
+  // koristimo HttpClient i Socket za komunikaciju sa serverom
+  // socket ce nam sluziti za primanje realtime obavestenja o statusu uploada i promenama u listi fajlova
+  // socket objekat se kreira i povezuje na server na URL: http://localhost:5000, naveden u konfiguraciji SocketIoModule u app.module.ts
   constructor(private http: HttpClient, private socket: Socket) { }
 
   getFileList(): Observable<{ files: string[] }> {
     return this.http.get<{ files: string[] }>('http://localhost:5000');
   }
 
+  // funkcija za upload fajla
   uploadFile(file: File): Observable<any> {
     const CHUNK_SIZE = 1024 * 1024; // 1MB chunks
     const totalChunks = Math.ceil(file.size / CHUNK_SIZE);
     let currentChunk = 0;
 
+    // funkcija koja rekurzivno uploaduje chunkove fajla
     const uploadChunk = (start: number): Promise<any> => {
       return new Promise((resolve, reject) => {
         const end = Math.min(start + CHUNK_SIZE, file.size);
@@ -28,15 +33,16 @@ export class FaApiService {
         formData.append('chunkIndex', currentChunk.toString());
         formData.append('totalChunks', totalChunks.toString());
 
+        // post metodom saljemo chunk na server
         this.http.post('http://localhost:5000', formData).subscribe({
+          // ako je chunk uspesno uploadovan, pozivamo sledeci chunk
           next: () => {
             currentChunk++;
-            const progress = Math.round((currentChunk / totalChunks) * 100);
-            this.socket.emit('status', { status: 'upload_progress', progress });
-
             if (currentChunk < totalChunks) {
+              // razresavamo Promise od slanja sledeceg chunka
               resolve(uploadChunk(end));
             } else {
+              // ako su svi chunkovi uploadovani, razresavamo Promise od uploada
               resolve('Upload complete');
             }
           },
@@ -65,7 +71,9 @@ export class FaApiService {
     return this.http.delete(`http://localhost:5000/${filename}`);
   }
 
+  // funkcija za realtime pracenje statusa uploada
   onStatusUpdate(): Observable<any> {
+    // koristimo fromEvent metodu Socket klase da bismo se pretplatili na dogadjaje sa servera
     return this.socket.fromEvent('status');
   }
 
